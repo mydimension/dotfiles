da94f1d658671ed6bf896b8d6a582b08781ee58e
William Cox <wcox@blackducksoftware.com>
2012-10-02 20:23:20 -0400

    another round of mutt refinements

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -7,7 +7,7 @@ def get_keychain_pass(account=None, server=None):
         'account': account,
         'server': server,
     }
-    command = "sudo %(security)s -v %(command)s -g -a %(account)s -s %(server)s" % params
+    command = "%(security)s -v %(command)s -g -a %(account)s -s %(server)s" % params
     output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
     outtext = [l for l in output.splitlines()
                if l.startswith('password: ')][0]
