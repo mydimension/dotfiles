add64c78d1094bfe3a531ac06839183d2eca38c9
William Cox <mydimension@gmail.com>
2016-09-12 01:51:56 -0400

    takeover amazon smile redirection in vimfx

--- vimfx/config.js
+++ vimfx/config.js
@@ -4,3 +4,9 @@ let map = (shortcuts, command, custom=false) => {
 
 map('',  'scroll_half_page_down');
 map('d', 'tab_close');
+
+vimfx.on('locationChange', ({vim, location}) => {
+    if (location.hostname.match(/^(?:www\.)?amazon\.com/)) {
+        vim.browser.loadURI(location.toString().replace(/:\/\/(?:www\.)?amazon\.com/, '://smile.amazon.com'));
+    }
+});
